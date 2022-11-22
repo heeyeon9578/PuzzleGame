43542cbe13fbb75bfc319f28728fa993d00b2bda
heeyeon9578 <heeyeon9578@naver.com>
2022-11-22 19:33:09 +0900

    1,2,3강

---
+++ Assets/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Dongle lastDongle;
+    public GameObject donglePrefab;
+    public Transform dongleGroup;
+    public int maxLevel;
+    private void Awake()
+    {
+        //프레임이 일정하게 하기
+        Application.targetFrameRate = 60;
+
+        //인스펙터의 interpolate: 이전 프레임을 비교하여 움직임을 부드럽게 보정
+    }
+    void Start()
+    {
+        NextDongle();
+    }
+    //다음 동글을 가져올 함수
+    void NextDongle()
+    {
+        Dongle newDongle = GetDongle();
+        lastDongle = newDongle;
+        lastDongle.gameManager = this;
+        //동글이의 레벨 값을 0~ maxLevel중에 랜덤으로 설정
+        lastDongle.level = Random.Range(0, maxLevel);
+        lastDongle.gameObject.SetActive(true);
+        StartCoroutine(WaitNext());
+    }
+    //동글이 떨어지기를 기다리는 코루틴 
+    IEnumerator WaitNext()
+    {
+        while(lastDongle != null)
+        {
+            yield return null;  
+        }
+        //yield return null; //한 프레임을 쉬는 코드
+        yield return new WaitForSeconds(2.5f); //2.5초 쉬는 코드
+        NextDongle();
+    }
+    //동글 생성 함수
+    Dongle GetDongle()
+    {
+        GameObject instant = Instantiate(donglePrefab, dongleGroup);
+        Dongle instantDongle = instant.GetComponent<Dongle>();
+        return instantDongle;
+
+    }
+    public void TouchDown()
+    {
+        if(lastDongle == null)
+        {
+            return;
+        }
+        lastDongle.Drag();
+    }
+
+    public void TouchUp()
+    {
+        if (lastDongle == null)
+        {
+            return;
+        }
+        lastDongle.Drop();
+        lastDongle = null;
+    }
+}
---
+++ Assets/Scripts/Dongle.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dongle : MonoBehaviour
+{
+    public int level;
+    public bool isDrag = false;
+    public bool isMerge =false;
+    Rigidbody2D rigid;
+    Animator anim;
+    CircleCollider2D circleCollider;    
+    public GameManager gameManager;
+    
+    private void Awake()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();  
+        anim = GetComponent<Animator>();   
+        rigid = GetComponent<Rigidbody2D>();    
+    }
+
+    //활성화 할 때마다 호출
+    private void OnEnable()
+    {
+        anim.SetInteger("Level", level);
+    }
+
+    void Update()
+    {
+        if (isDrag)
+        {
+            //마우스의 포지션 받아오기
+            //스크린 좌표계 - 캔버스
+            //월드 좌표계 - 게임 속
+            //스크린 좌표계를 월드 좌표계로 바꾸는 함수 -> ScreenToWorldPoint
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float leftBorder = -4.2f + transform.localScale.x / 2f;
+            float rightBorder = 4.2f - transform.localScale.x / 2f;
+
+            //x,y,z축 고정
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
+            mousePos.y = 8;
+            mousePos.z = 0;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);// Lerp -> 0~1 사이 값만 가능
+        }
+      
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+    }
+    public void Drop()
+    {
+        isDrag = false;
+        //인스펙터 창에서는 꺼두기
+        //물리작용을 받게 하기
+        rigid.simulated = true;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Dongle")
+        {
+            Dongle other = collision.gameObject.GetComponent<Dongle>();
+            //동글 합치기 로직 실행
+            if (level == other.level&&!isMerge &&!other.isMerge&&level <7)
+            {
+                //나와 상대편 위치 가져오기
+                //나의 위치
+                float meX = transform.position.x;
+                float meY = transform.position.y;
+                //상대편의 위치
+                float otherX = other.transform.position.x;   
+                float otherY = other.transform.position.y;  
+                //1.내가 아래에 있을 때
+                //2.동일한 높이일 때, 내가 오른쪽에 있을때
+                if(meY<otherY || (meY==otherY && meX > otherX))
+                {
+                    //상대방은 숨기기
+                    other.Hide(transform.position);
+                    //나는 레벨업
+                    LevelUp();
+                }
+            }
+        }
+    }
+
+    public void Hide(Vector3 targetPos)
+    {
+        isMerge = true;
+        rigid.simulated = false;
+        circleCollider.enabled = false;
+
+        StartCoroutine(HideRoutine(targetPos));
+    }
+
+    IEnumerator HideRoutine(Vector3 targetPos)
+    {
+        int frameCount = 0;
+        while(frameCount < 20) //마치 업데이트처럼 작동
+        {
+            frameCount++;
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.5f);
+            yield return null;
+
+        }
+        isMerge = false;
+        gameObject.SetActive(false);
+    }
+    //나는 레벨업
+    void LevelUp()
+    {
+        isMerge = true;
+        //레벨업 중에 방해가 될 수 있는 물리속도를 제거하기
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0;
+        StartCoroutine(LevelUpRoution());
+
+    }
+
+    IEnumerator LevelUpRoution()
+    {
+        yield return new WaitForSeconds(0.2f);
+        anim.SetInteger("Level", level + 1);
+        yield return new WaitForSeconds(0.3f);
+        level++;    
+        gameManager.maxLevel =Mathf.Max(gameManager.maxLevel,level);    
+        isMerge = false;    
+
+    }
+}
